169da6e31495766c108fd49861a341cbdbf264f3
bsanderson11 <bill@towniebagewls.com>
2020-08-30 17:00:03 -0700

    finished cp 15 search

--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q # needed for advance search use of  :  and or
+
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, # check logged in
     PermissionRequiredMixin #new add class permissions 
@@ -24,3 +26,31 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login' #new 
     permission_required = 'books.special_status' # support adding admin permissions 
+
+class SearchResultsListView(ListView): #new
+    
+    model = Book
+
+    context_object_name = 'book_list'
+
+    template_name = 'books/search_results.html'
+
+    #queryset = Book.objects.filter(title__icontains='beginners') #new
+    def get_queryset(self): # new
+       query = self.request.GET.get('q')
+       return Book.objects.filter(
+           Q(title__icontains=query) | Q(author__icontains=query)
+       )
+
+
+
+"""  model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+ """
