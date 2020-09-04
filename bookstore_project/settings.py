baf85585d6f5a07ede057d72a029d9a299ad3004
bsanderson11 <bill@towniebagewls.com>
2020-09-04 16:40:43 -0700

    updatre dj-database-url

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -221,5 +221,6 @@ if ENVIRONMENT == 'production':
     SECURE_REFERRER_POLICY = 'same-origin'
    
 #Heroku Setup DB
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
