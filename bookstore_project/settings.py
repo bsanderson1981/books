c745353b6f9eb478ad073226f830dc22b7f50fc9
bsanderson11 <bill@towniebagewls.com>
2020-09-04 17:02:45 -0700

    added allow host for heroku

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('YML_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
-ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['peaceful-basin-36305.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
